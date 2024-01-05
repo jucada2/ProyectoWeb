af4f3d386ab0f3310dc1f9b479c664dd6989c3d5
profePython <profe@profe.com>
2024-01-05 17:14:21 -0500

    proyecto vacio del after 5 enero

--- AppPeliculas/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from AppPeliculas.models import *
-
-# Register your models here.
-admin.site.register(Serie)
-admin.site.register(Pelicula)
-admin.site.register(Futbol)
--- AppPeliculas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApppeliculasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppPeliculas'
